0709ba36d540d119316ee3c5a0e89e4ede143861
Hasan Sadeghi <sadeghihasi@gmail.com>
2024-02-24 02:57:01 +0330

    extract authers!

--- script.py
+++ script.py
@@ -1,11 +1,45 @@
-# script.py
+import argparse
+import requests
+from lxml import html
+from bs4 import BeautifulSoup
 from libgen.models import Keyword
 
 
-word = ""
+def crawl_libgen(keyword):
+    URL = f"https://www.libgen.is/search.php?req={keyword}&open=0&res=25&view=detailed"
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    root = html.fromstring(str(soup))
+    with open(f"{keyword}.html", "w") as f:
+        f.write(html.tostring(root, pretty_print=True).decode())
+    books = root.xpath('/html/body/table/font/table')
+    print(books)
+    for book in books:
+        authers = book.xpath('./tbody/tr[3]/td[2]')
+        # Get texts from selected elements
+        texts = [auther.text_content() for auther in authers]
+        # Print the texts
+        for text in texts:
+            print(text)
 
 
-URL = f"https://www.libgen.is/search.php?req={word}&lg_topic=libgen&open=0&view=simple&res=25&phrase=1&column=def"
+if __name__ == "__main__":
+    # Parsing command-line arguments
+    parser = argparse.ArgumentParser(
+        description='Keyword to search in the Libgen site'
+    )
+    parser.add_argument(
+        '--keyword',
+        '-k',
+        required=True,
+        help='Keyword to search in the Libgen site!'
+    )
+    args = parser.parse_args()
+
+    if args.keyword:
+        crawl_libgen(args.keyword)
+    else:
+        print("No action specified. Use --keyword or -k.")
 
 
 def perform_database_operations():
@@ -13,7 +47,3 @@ def perform_database_operations():
     queryset = Keyword.objects.all()
     for obj in queryset:
         print(f'Title: {obj.title}, Content: {obj.content}')
-
-
-if __name__ == "__main__":
-    perform_database_operations()
